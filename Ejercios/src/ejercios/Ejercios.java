520cff5ec1d7cf56f3f138111b079fc28800ac23
Duvan Montoya <dubis310@gmail.com>
2022-06-22 15:31:32 -0500

    se Crea una carpeta que va a estar llena de ejercicios de la universidad UdeA

---
+++ Ejercios/src/ejercios/Ejercios.java
@@ -0,0 +1,16 @@
+package ejercios;
+
+import java.util.Scanner;
+
+public class Ejercios {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int rest= 0;
+        
+        rest = 8%3;
+        System.out.println(rest);
+    }
+    
+}
